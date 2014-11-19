77270045b25c6ccaa207cf5767415ce831cc0b09
Dimitry <alegria2112@gmail.com>
2014-11-19 08:29:11 +0000

    refactoring easter.rb

--- easter.rb
+++ easter.rb
@@ -1,11 +1,8 @@
 module Orthodox 
   def orthodox(year)
     year = yield
-    a = year % 19
-    b = year % 4
-    c = year % 7
-    d = (19*a + 15) % 30
-    e = (2*b + 4*c + 6*d +6) % 7
+    d = ((19*(year % 19) + 15) % 30) 
+    e = (2*(year % 4) + 4*(year % 7) + 6*d + 6) % 7
     f = d + e
     if f<=9 
       day = 22+f + (13 if year > 1918)
@@ -25,3 +22,5 @@ module Orthodox
     puts "| Year: #{year}\n| Date: #{day} of #{month}."
   end
 end
+
+
